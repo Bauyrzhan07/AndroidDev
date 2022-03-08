67a7bf6c592db756ca4c9f8bff9c85c487534e49
Bauyrzhan07 <qwe123ez@gmail.com>
2022-03-08 23:58:22 +0600

    calculator made with constraints

--- lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,21 +1,45 @@
 package com.example.lab1
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.text.isDigitsOnly
 import com.example.lab1.databinding.ActivityMainBinding
 
-private lateinit var binding: ActivityMainBinding
-
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     var prevValue: Double? = null
     var curOperation: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnDot.setOnClickListener{ setContent(".")}
+
+        initUI()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("curValue", binding.curVal.text.toString())
+        if(prevValue != null)
+            outState.putString("prevValue", prevValue.toString())
+        if(curOperation != null)
+            outState.putString("curOperation", curOperation)
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        binding.curVal.text = savedInstanceState.getString("curValue")
+        if(savedInstanceState.containsKey("prevValue"))
+            prevValue = savedInstanceState.getString("prevValue")?.toDouble()
+        if(savedInstanceState.containsKey("curOperation"))
+            curOperation = savedInstanceState.getString("curOperation")
+    }
+
+    private fun initUI() {
+        binding.btnDot.setOnClickListener{ if(binding.curVal.text.isNotEmpty()) setContent(".")}
         binding.btn0.setOnClickListener { setContent("0") }
         binding.btn1.setOnClickListener { setContent("1") }
         binding.btn2.setOnClickListener { setContent("2") }
@@ -26,10 +50,10 @@ class MainActivity : AppCompatActivity() {
         binding.btn7.setOnClickListener { setContent("7") }
         binding.btn8.setOnClickListener { setContent("8") }
         binding.btn9.setOnClickListener { setContent("9") }
-        binding.btnDiv.setOnClickListener { calc("div") }
-        binding.btnMult.setOnClickListener { calc("mult") }
-        binding.btnPlus.setOnClickListener { calc("plus") }
-        binding.btnSub.setOnClickListener { calc("minus") }
+        binding.btnDiv.setOnClickListener { calc("divide") }
+        binding.btnMult.setOnClickListener { calc("multiply") }
+        binding.btnAdd.setOnClickListener { calc("add") }
+        binding.btnSubstr.setOnClickListener { calc("subtract") }
         binding.btnEqual.setOnClickListener { calc("equals")}
         binding.btnDelete.setOnClickListener{
             if(binding.curVal.text.isNotEmpty()) {
@@ -45,27 +69,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun Double.round(decimals: Int = 2): Double = "%.${decimals}f".format(this).toDouble()
 
-    private fun calc(operation:String){
-        if(binding.curVal.text.isEmpty())
-            return
-        var currentValue = binding.curVal.text.toString().toDouble()
+    private fun calc(operation: String) {
+        if(binding.curVal.text.isEmpty()) return
+        val currentValue = binding.curVal.text.toString().toDouble()
         binding.curVal.text = ""
         if(prevValue == null && curOperation == null && operation != "equals"){
             prevValue = currentValue
             curOperation = operation
-        }else{
+        } else {
             var result = when (curOperation) {
-                "div" -> (prevValue!! / currentValue).toString()
-                "mult" -> (prevValue!! * currentValue).toString()
-                "plus" -> (prevValue!! + currentValue).toString()
-                "minus" -> (prevValue!! - currentValue).toString()
+                "divide" -> (prevValue!! / currentValue).toString()
+                "multiply" -> (prevValue!! * currentValue).toString()
+                "add" -> (prevValue!! + currentValue).toString()
+                "subtract" -> (prevValue!! - currentValue).toString()
                 else -> ""
             }
             result = when {
-                result.length > 1 && result[result.length - 1] == '0' -> result.subSequence(
-                    0,
-                    result.length - 2
-                ) as String
+                result.length > 1 && result[result.length - 1] == '0' -> {
+                    result.subSequence(0, result.length - 2) as String
+                }
                 result != "" -> result.toDouble().round(6).toString()
                 else -> ""
             }
